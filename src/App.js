4ad47c1fc67177ddf65536a63babb146415e28fd
Mviita65 <mviita65@gmail.com>
2020-10-21 22:38:04 +0300

    Saa kaatumaan jos pari operandia peräkkäiin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+
+  const [nakyma,setNakyma]=useState(0)
+   
+  const nayta = (painallus) => {
+    switch(painallus){
+      case "C":{
+        setNakyma(String(0))
+        break;
+      }
+      case "=":{
+        let x = eval(nakyma)
+        setNakyma(String(x))
+        break;
+      }
+      default:{
+        if (nakyma == 0){
+          setNakyma(painallus)
+        } else {
+          let naytto = String(nakyma)
+          let uusiNaytto = naytto.concat(painallus)
+          setNakyma(uusiNaytto)
+        }
+        break;
+      }
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{nakyma}</p>
+      <p>
+        <button onClick={()=>nayta(0)}>0</button>
+        <button onClick={()=>nayta(1)}>1</button>
+        <button onClick={()=>nayta(2)}>2</button>
+        <button onClick={()=>nayta(3)}>3</button>
+        <button onClick={()=>nayta(4)}>4</button>
+        <button onClick={()=>nayta("(")}>(</button>
+        <br/>
+        <button onClick={()=>nayta(5)}>5</button>
+        <button onClick={()=>nayta(6)}>6</button>
+        <button onClick={()=>nayta(7)}>7</button>
+        <button onClick={()=>nayta(8)}>8</button>
+        <button onClick={()=>nayta(9)}>9</button>
+        <button onClick={()=>nayta(")")}>)</button>
+        <br/>
+        <button onClick={()=>nayta("+")}>+</button>
+        <button onClick={()=>nayta("-")}>-</button>
+        <button onClick={()=>nayta("*")}>*</button>
+        <button onClick={()=>nayta("/")}>/</button>
+        <button onClick={()=>nayta("=")}>=</button>
+        <button onClick={()=>nayta("C")}>C</button>
+      </p>
     </div>
   );
 }
