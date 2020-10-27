903e9b71e2dd6da17b6f74f953c39d13c2b3df33
unknown <mviita65@gmail.com>
2020-10-27 18:42:32 +0200

    Muotoilua

--- reactlists/src/App.js
+++ reactlists/src/App.js
@@ -7,7 +7,6 @@ function App() {
 
 const [listaA,setListaA]=useState(["Jukka","Pekka","Kalle","Saara","Katriina","Susanna"])
 const [listaB,setListaB]=useState(["Jorma","Jaakko","Antti"])
-// const [listaB,setListaB]=useState(["This list has no items"])
 const [valittu,setValittu]=useState("")
 
 
@@ -17,7 +16,6 @@ const lajittelu = (lista) => {
       let kopioListaA = [...listaA]
       kopioListaA.sort()
       setListaA(kopioListaA)
-      // setLajiteltu(!lajiteltu)
       console.log(kopioListaA)
       break;
     }
@@ -25,7 +23,6 @@ const lajittelu = (lista) => {
       let kopioListaB = [...listaB]
       kopioListaB.sort()
       setListaB(kopioListaB)
-      // setLajiteltu(!lajiteltu)
       console.log(kopioListaB)
       break;
     } 
@@ -36,7 +33,7 @@ const lajittelu = (lista) => {
   }
 }
 
-const merkitse = (lista,index) => {
+const merkitse = (lista,index) => { // tekee listasta ja indeksista merkkijonon esim. listaA[0]
   let listanNimi = lista
   let listanIndex = index
   let merkitty=`${listanNimi}[${listanIndex}]`
@@ -45,49 +42,45 @@ const merkitse = (lista,index) => {
 }
 
 const siirra = (mihin) =>{
-  let lista = valittu.slice(0,6)
-  let index = Number(valittu.slice(7,8))
+  let lista = valittu.slice(0,6) // saa merkkijonon alusta kuusi merkkiä esim. listaA
+  let index = Number(valittu.slice(7,8)) // saa merkkijonosta indexin esim. 0
   let siirrettava = ""
   let i = 0
-  console.log(lista)
-  console.log(index)
   switch (lista){
-    case "listaA":{
+    case "listaA":{   
       let uusiListaA = []
-      let uusiListaB = [...listaB]    
-      if (mihin==="toRight"){
-        console.log("Siirrä "+valittu+" B listalle")
-        let pituus = listaA.length
-        for (i = 0; i < pituus+1; i++){
-          if (i !== index) {           
-            uusiListaA.push(listaA[i])
-            setListaA(uusiListaA)
+      let uusiListaB = [...listaB]  // listaA:n ollessa kysymyksessä kopidaan B siirrettävän lisäystä varten  
+      if (mihin==="toRight"){       // ollaan siirtämässä listalta A listalle B
+        let pituus = listaA.length    // kuinka monta nimeä on listassa
+        for (i = 0; i < pituus+1; i++){ // käydään lista läpi
+          if (i !== index) {            // jos kyseessä ei ole siirrettävän indeksi
+            uusiListaA.push(listaA[i])  // kerätään nimi listalle
           } else {
-            siirrettava = listaA[i]
+            siirrettava = listaA[i]     // jos on kyseessä siirrettävä indeksi otetaan nimi talteen
           }
         }
-        uusiListaB.push(siirrettava)
-        setListaB(uusiListaB)
+        uusiListaB.push(siirrettava)    // lisätään talteen otettu nimi listalle B
+        setListaA(uusiListaA)           // päivitetään lista A
+        setListaB(uusiListaB)           // päivitetaan lista B
         break;
       }
       break;
     }
-    case "listaB":{
-      let uusiListaA = [...listaA]
-      let uusiListaB = []
+    case "listaB":{                     // sama kuin yllä, mutta nyt siirto listalta B listalle A
+      let newListaA = [...listaA]
+      let newListaB = []
       if (mihin==="toLeft"){
-        console.log("Siirrä "+valittu+" A listalle") 
         let pituus = listaB.length
         for (i = 0; i < pituus+1; i++){
           if (i !== index) {           
-            uusiListaB.push(listaB[i])
-            setListaB(uusiListaB)
+            newListaB.push(listaB[i])
           } else {
             siirrettava = listaB[i]
           }
         }
-        uusiListaA.push(siirrettava)
-        setListaA(uusiListaA)
+        newListaA.push(siirrettava)
+        setListaA(newListaA)
+        setListaB(newListaB)
         break;
       }
       break;
@@ -102,16 +95,32 @@ const siirra = (mihin) =>{
   return (
     <div className="grid-container">
       <div className="grid-lista">
-        <header>List_A <span onClick={()=>{lajittelu("A")}}>| sort</span></header>
-        {listaA.map((item,index)=><div className="lista-item" key={index} onClick={()=>{merkitse("listaA",index)}}>{listaA[index]}</div>)}
+        <header>List_A 
+          <span onClick={()=>{lajittelu("A")}}> | sort </span>
+          <span> | filter </span>
+        </header>
+        {listaA.map((item,index)=>
+        <div className="lista-item" key={index} onClick={()=>{
+          merkitse("listaA",index)}}>{listaA[index]}
+        </div>)}
       </div>
       <div className="grid-siirrot">
-         <div className="siirrot-item" onClick={()=>{siirra("toRight")}}>»</div>
-         <div className="siirrot-item" onClick={()=>{siirra("toLeft")}}>«</div>
-     </div>
+        <div className="siirrot-item" onClick={()=>{
+          siirra("toRight")}}>»
+        </div>
+        <div className="siirrot-item" onClick={()=>{
+          siirra("toLeft")}}>«
+        </div>
+      </div>
       <div className="grid-lista">
-        <header>List_B <span onClick={()=>{lajittelu("B")}}>| sort</span></header>
-        {listaB.map((item,index)=><div className="lista-item" key={index} onClick={()=>{merkitse("listaB",index)}}>{listaB[index]}</div>)}
+        <header>List_B 
+          <span onClick={()=>{lajittelu("B")}}> | sort </span>
+          <span> | filter </span>
+        </header>
+        {listaB.map((item,index)=>
+        <div className="lista-item" key={index} onClick={()=>{
+          merkitse("listaB",index)}}>{listaB[index]}
+        </div>)}
       </div>
     </div>
   );
