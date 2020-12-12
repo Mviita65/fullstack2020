895f97bc9713cde2f71bc1dd1781834b3be1a093
Mviita65 <mviita65@gmail.com>
2020-12-12 15:28:45 +0200

    Refactoroitu App

---
+++ tentti/client/src/components/kysymykset.js
@@ -0,0 +1,43 @@
+import {
+    muutaKysymys,
+    lisaaKysymys,
+    poistaKysymysTentilta,
+  } from './dataManipulation.js';
+import Vaihtoehdot from './vaihtoehdot.js';
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
+
+function Kysymykset(props) {  //näytölle tentin kysymykset ja kutsuu Vaihtoehdot näyttämään kysymysten vaihtoehdot
+
+    return <section>
+      {props.hallinta ? props.data.kysymykset.map((item, kysymysIndex) =>     // jos hallinta valittu
+        <div key={item.kysymysid} className="kysymys">
+          <input type="text" value={item.kysymys} onChange={(event) =>{       // kysymystä voidaan muotoilla
+            muutaKysymys(event,props,kysymysIndex)}}>
+          </input> <button className="delButton" onClick={()=>{               // kysymys voidaan poistaa
+            if (window.confirm("Poistetaanko kysymys ("+props.data.kysymykset[kysymysIndex].kysymys+") tentiltä?")){
+              poistaKysymysTentilta(props,kysymysIndex)
+            }
+          }}><DeleteTwoToneIcon /></button> 
+          <Vaihtoehdot dispatch={props.dispatch} tenttiIndex={props.tenttiIndex} tenttiid={props.data.tenttiid} kysymysIndex={kysymysIndex} 
+            data={props.data.kysymykset[kysymysIndex]} vastaukset={props.vastaukset} setVastaukset={props.setVastaukset} hallinta={props.hallinta} setHallinta={props.setHallinta}
+          />
+        </div>):                                                              // tentti menossa
+        props.data.kysymykset.map((item, kysymysIndex) =>
+          <div key={item.kysymysid} className="kysymys">{item.kysymys}
+          <Vaihtoehdot dispatch={props.dispatch} tenttiIndex={props.tenttiIndex} tenttiid={props.data.tenttiid} kysymysIndex={kysymysIndex} 
+            data={props.data.kysymykset[kysymysIndex]} vastaukset={props.vastaukset} setVastaukset={props.setVastaukset} hallinta={props.hallinta} setHallinta={props.setHallinta}
+          />
+      </div>)}
+      {props.hallinta ? <div className="add"><span className="add-item" onClick={()=>{
+          // jos hallintatila, voi lisätä uuden kysymyksen
+        lisaaKysymys(props)}}> + </span>
+      </div> : ""}
+      <div>
+        {props.hallinta ? "" : <div><span className="button" onClick={()=>         // jos tentti menossa, voi valita tai piilottaa oikeiden vastausten näytön
+          props.setVastaukset(!props.vastaukset)}>Vastaukset</span> <span className="button" onClick={()=>{         // jos tentti menossa, voi valita tai piilottaa oikeiden vastausten näytön
+            props.setKaaviot(1)}}>Kaaviot</span></div> }
+      </div>
+    </section>
+  }
+
+  export default Kysymykset
---
+++ tentti/client/src/components/vaihtoehdot.js
@@ -0,0 +1,43 @@
+import {
+    muutaVaihtoehtoTeksti,
+    muutaVaihtoehtoArvo,
+    vastausAnnettu,
+    lisaaVaihtoehto,
+    poistaVaihtoehto
+  } from './dataManipulation.js'
+import cathead from './img/cathead.jpg'
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
+
+function Vaihtoehdot(props) { // näytölle kysymysten vaihtoehdot ja reagointi jos merkitään vastaukseksi
+
+    return <section>
+        {props.hallinta ? props.data.vaihtoehdot.map((item, veIndex) =>           // jos hallinta valittu
+          <div key={item.vaihtoehtoid} className="vastaus">
+            <input type="checkbox" checked={item.korrekti} onChange={(event) => { // voidaan muuttaa mikä on oikea vaihtoehto
+                muutaVaihtoehtoArvo(event,props,veIndex)}}></input>
+            <input type="text" value={item.vaihtoehto} onChange={(event) =>{          // voidaan muotoilla vaihtoehdon tekstiä
+                muutaVaihtoehtoTeksti(event,props,veIndex)}}> 
+            </input> <button className="delButton" onClick={()=>{                 // voidaan poistaa vaihtoehto
+              if (window.confirm("Poistetaanko vaihtoehto ("+props.data.vaihtoehdot[veIndex].vaihtoehto+")?")){
+                poistaVaihtoehto(props,veIndex)
+              }
+            }}><DeleteTwoToneIcon /></button> {!props.hallinta && item.valittu && item.korrekti ? <img alt="cathead" src={cathead}/> : ""}
+          </div>):                                                                // muu kuin hallintatila
+          props.vastaukset ? props.data.vaihtoehdot.map((item, veIndex) =>        // oikeiden vastausten näyttö valittu: valintoja ei voi muuttaa 
+            <div key={item.vaihtoehtoid} className="vastaus">
+              <input type="checkbox" checked={item.valittu} readOnly></input>            
+              <input type="checkbox" checked={item.korrekti} readOnly></input>
+              {item.vaihtoehto} {item.valittu && item.korrekti ? <img alt="cathead" src={cathead}/> : ""}
+          </div>) :
+            props.data.vaihtoehdot.map((item, veIndex) =>                         // tentti menossa (vastaukset poissa)
+              <div key={item.vaihtoehtoid} className="vastaus">
+                <input type="checkbox" checked={item.valittu} onChange={(event) => {  // vaihtoehto voidaan valita vastaukseksi tai poistaa
+                  vastausAnnettu(event,props,veIndex)}}>
+                </input> {item.vaihtoehto}
+            </div>)}
+            {props.hallinta ? <div className="add"><span className="add-ve" onClick={()=>{  // jos hallintatila, voi lisätä uuden vaihtoehdon
+              lisaaVaihtoehto(props)}}> + </span></div> : ""}
+      </section>
+    }
+
+    export default Vaihtoehdot
