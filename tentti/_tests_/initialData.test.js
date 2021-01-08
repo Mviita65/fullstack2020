73907d7748a4f19be6fc977881b037cac1336663
Mviita65 <mviita65@gmail.com>
2021-01-08 15:17:38 +0200

    Jest-testit talteen

--- tentti/_tests_/initialData.test.js
+++ tentti/_tests_/initialData.test.js
@@ -1,10 +1,35 @@
 const {initialData} = require('../client/src/components/initialData')
 
-describe.skip('Alkudatatestit', ()=>{
-    
-    test('Kurssilta pitää löytyä ainakin yksi tentti', () => {
+
+describe('Alkudatatestit', ()=>{
+
+    test('Alkudata on olemassa', () => {
         expect(initialData.length).toBeGreaterThan(0);
     })
 
+    test('Alkudatan tentillä pitää olla useampi kuin yksi kysymys', () => {
+        expect(initialData[0].kysymykset.length).toBeGreaterThan(1)
+    })
 
+    test('Tentin jokaiselle kysymykselle on oikea vaihtoehto', () => {
+        let kysymyslaskuri = 0
+        let oikeatlaskuri = 0
+        let verranto = 0
+        initialData[0].kysymykset.forEach(item => {
+            kysymyslaskuri++
+            let oikeaOn = false
+            item.vaihtoehdot.forEach(choice => {
+                if (choice.korrekti === 1 && !oikeaOn) {
+                    oikeatlaskuri++
+                    oikeaOn = true
+                }
+            })    
+        })
+        verranto = oikeatlaskuri/kysymyslaskuri
+        expect(verranto).toEqual(1)
+    })     
+    
+    test('Onko alkudatassa TENTTI B', () => {
+        expect(initialData[0].tentti).toMatch(/TENTTI B/)
+    })
 })
